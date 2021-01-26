63c5b28212c523fdc3390d87b345e4b6cc1701b5
Lohan Luiz <lohanluiz12@gmail.com>
2021-01-26 18:48:55 -0400

    adding components

---
+++ next-tailwind-framermotion/components/Header.tsx
@@ -0,0 +1,9 @@
+const Header: React.FC = () => {
+  return (
+    <header className="flex justify-center py-2">
+      <img src="./img/logo.svg" alt="logo" />
+    </header>
+  )
+}
+
+export default Header;
